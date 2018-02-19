93c1ef56593db6e2f19fc56ca8c0fb8bebb6bcb1
guidomontufar <montufar@mis.mpg.de>
2018-02-18 18:18:47 -0800

    minor cleaning in tests/test_dit_dataPs.py

--- python/tests/test_dit_dataPs.py
+++ python/tests/test_dit_dataPs.py
@@ -1,4 +1,4 @@
-# Generates the datapoints for dit in Figure 3 of the paper
+# Generates the data points for dit in Figure 3 of the paper
 import sys
 sys.path.insert(0, '../')
 from admUI import computeQUI
@@ -33,50 +33,51 @@ npy = np.array(mat['Ps'])
 nsmax = 5
 ndist = 100
 
-UIv = np.empty(shape=(ndist,nsmax))
-ltimev = np.empty(shape=(ndist,nsmax))
-UIpv = np.empty(shape=(ndist,nsmax))
-ltimepv = np.empty(shape=(ndist,nsmax))
+UIv = np.empty(shape=(ndist, nsmax))
+ltimev = np.empty(shape=(ndist, nsmax))
+UIpv = np.empty(shape=(ndist, nsmax))
+ltimepv = np.empty(shape=(ndist, nsmax))
 dit_errorcnt = 0
-UIcv = np.empty(shape=(ndist,nsmax))
-ltimecv = np.empty(shape=(ndist,nsmax))
+UIcv = np.empty(shape=(ndist, nsmax))
+ltimecv = np.empty(shape=(ndist, nsmax))
 
-for ns in range(1,nsmax):
-    ny=ns; nz=ns;
+
+for ns in range(1, nsmax):
+    ny = ns; nz = ns;
     print("--------------- ns= %s ---------------" %(ns+1))
-    for i in range(0,ndist):
-        Pt = npy[:,i,ns]
+    for i in range(0, ndist):
+        Pt = npy[:, i, ns]
         P = Pt[Pt!=0]
-        Ps = P.reshape(nz+1,ny+1,ns+1)
+        Ps = P.reshape(nz+1, ny+1, ns+1)
         d = Distribution.from_ndarray(Ps)
         d.set_rv_names('SXY')
-        print(i)
+        print(i, '/', ndist-1)
     
         try:
             pid = algorithms.pid_broja(d, ['X', 'Y'], 'S')
         except dit.exceptions.ditException:
             print("ditException: P = ", P, ", ns=ny=nz=", ns+1, ", i=", i)
             dit_errorcnt = dit_errorcnt+1
-            UIv[i,ns] = 0
-            ltimev[i,ns] = 0  
-            UIpv[i,ns] = 0
-            ltimepv[i,ns] = 0  
+            UIv[i, ns] = 0
+            ltimev[i, ns] = 0
+            UIpv[i, ns] = 0
+            ltimepv[i, ns] = 0
             continue
         else:
             start_timep = time.time()
             pid = algorithms.pid_broja(d, ['X', 'Y'], 'S')
             UIXp = pid.U0
             lapsed_timep = time.time() - start_timep
-            UIpv[i,ns] = UIXp
-            ltimepv[i,ns] = lapsed_timep 
+            UIpv[i, ns] = UIXp
+            ltimepv[i, ns] = lapsed_timep
             print("ditUI = %.15f" %UIXp, "      time = %.15f" %lapsed_timep)
 
 np.set_printoptions(precision=15)
 print("-------------------- ditUI --------------------")
-UIpv = np.delete(UIpv,0,1)
-mUIpv = np.mean(UIpv,axis=0)
-ltimepv = np.delete(ltimepv,0,1)
-mltimepv = np.mean(ltimepv,axis=0)
+UIpv = np.delete(UIpv, 0, 1)
+mUIpv = np.mean(UIpv, axis=0)
+ltimepv = np.delete(ltimepv, 0, 1)
+mltimepv = np.mean(ltimepv, axis=0)
 print("ditUI:")
 print(mUIpv)
 print("time:")
