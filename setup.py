270761d56628ec4f836b31fc6f3e23b1bb96435d
heytitle <pat.chormai@gmail.com>
2020-01-31 12:44:55 +0100

    update package description and extras_requires

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup
 
 setup(
-    name="infodecom",
+    name="admUI",
     version="1.0",
-    description="A useful module",
+    description="This package provides functions to compute the function UI introduced in by Bertschinger et al. [\"Quantifying unique information\", Entropy 2014, 16(4), 2161-2183].",
     author="Man Foo",
     author_email="foomail@foo.com",
     packages=["admUI"],
     package_dir={"admUI": "python"},
     install_requires=[
         "numpy",
-        "dit"
-    ], #external packages as dependencies
+    ],
+    extras_require = {
+        "dit": ["dit>=1.2.3"]
+    }
 )
